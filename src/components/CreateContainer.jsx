ec79df9c6651a00cae050c36207f6e7c337b7fa8
Kristaps Tunnis <74448912+Kriforce@users.noreply.github.com>
2023-05-02 08:47:58 +0300

    Add files via upload

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
-import { MdFastfood } from 'react-icons/md'
+import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from 'react-icons/md'
 
+import { deleteObject, getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { storage } from "../firebase.config";
+
+import Loader from "./Loader";
 import { categories } from "../utils/data";
+import { getAllFoodItems, saveItem } from "../utils/firebaseFunctions";
 
 const CreateContainer = () => {
   const [title, setTitle] = useState('');
@@ -14,6 +19,109 @@ const CreateContainer = () => {
   const [alertStatus, setAlertStatus] = useState('danger');
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const uploadImage = (e) => {
+    setIsLoading(true);
+    const imageFile = e.target.files[0];
+    const storageRef = ref(storage, `Images/${Date.now()}-${imageFile.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const uploadProgress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      },
+      (error) => {
+        console.log(error);
+        setFields(true);
+        setMsg("Error while uploading : Please, try again");
+        setAlertStatus("danger");
+        setTimeout(() => {
+          setFields(false);
+          setIsLoading(false);
+        }, 4000);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageAsset(downloadURL);
+          setIsLoading(false);
+          setFields(true);
+          setMsg("Image uploaded successfully");
+          setAlertStatus("success");
+          setTimeout(() => {
+            setFields(false);
+          }, 4000);
+        });
+      }
+    );
+  };
+
+  const deleteImage = () => {
+    setIsLoading(true);
+    const deleteRef = ref(storage, imageAsset);
+    deleteObject(deleteRef).then(() => {
+      setImageAsset(null);
+      setIsLoading(false);
+      setFields(true);
+      setMsg("Image deleted successfully");
+      setAlertStatus("success");
+      setTimeout(() => {
+        setFields(false);
+      }, 4000);
+    });
+  };
+
+  const saveDetails = () => {
+    setIsLoading(true);
+    try {
+      if ((!title || !calories || !imageAsset || !price || !category)) {
+        setFields(true);
+        setMsg("Required fields can't be empty");
+        setAlertStatus("danger");
+        setTimeout(() => {
+          setFields(false);
+          setIsLoading(false);
+        }, 4000);
+      } else {
+        const data = {
+          id: `${Date.now()}`,
+          title: title,
+          imageURL: imageAsset,
+          category: category,
+          calories: calories,
+          qty: 1,
+          price: price,
+        };
+        saveItem(data);
+        setIsLoading(false);
+        setFields(true);
+        setMsg("Data uploaded successfully");
+        setAlertStatus("success");
+        setTimeout(() => {
+          setFields(false);
+        }, 4000);
+        clearData();
+      }
+    } catch (error) {
+      console.log(error);
+      setFields(true);
+      setMsg("Error while uploading : Please, try again");
+      setAlertStatus("danger");
+      setTimeout(() => {
+        setFields(false);
+        setIsLoading(false);
+      }, 4000);
+    }
+  };
+
+  const clearData = () => {
+    setTitle("");
+    setImageAsset(null);
+    setCalories("");
+    setPrice("");
+    setCategory("Select category");
+  };
 
   return (
     <div className='w-full  min-h-screen flex items-center justify-center'>
@@ -43,14 +151,14 @@ const CreateContainer = () => {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             placeholder='Give me a title...'
-            className='w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor' 
+            className='w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor' 
           />
         </div>
 
         <div className='w-full'>
           <select 
             onChange={(e) => setCategory(e.target.value)}
-            className='outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer'
+            className='outline-none w-full text-base border-2 border-gray-200 p-2 rounded-md cursor-pointer'
           >
             <option value="other" className='bg-white'>Select Category</option>
             {
@@ -68,7 +176,76 @@ const CreateContainer = () => {
         </div>
 
         <div className='group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg'>
-          
+          {isLoading ? <Loader /> : <>
+            {
+              !imageAsset ? (
+                <>
+                  <label className='w-full h-full flex flex-col items-center justify-center cursor-pointer'>
+                    <div className='w-full h-full flex flex-col items-center justify-center gap-2'>
+                      <MdCloudUpload className='text-gray-500 text-3xl hover:text-gray-700' />
+                      <p className='text-gray-500 hover:text-gray-700'>Click here to upload</p>
+                    </div>
+                    <input 
+                      type="file" 
+                      name='uploadImage' 
+                      accept='image/*' 
+                      onChange={uploadImage} 
+                      className='w-0 h-0' 
+                    />
+                  </label>
+                </> 
+              ) : (
+                <>
+                  <div className='relative h-full'>
+                    <img src={imageAsset} alt="upload image" className='w-full h-full object-cover' />
+                    <button
+                      type='button'
+                      className='absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out'
+                      onClick={deleteImage}
+                    >
+                      <MdDelete className='text-white' />
+                    </button>
+                  </div>
+                </>
+              )}
+            </>
+          }
+        </div>
+
+        <div className='w-full flex flex-col md:flex-row items-center gap-3'>
+          <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+            <MdFoodBank className='text-gray-700 text-2xl' />
+            <input 
+              type="text"
+              required
+              value={calories}
+              onChange={(e) => setCalories(e.target.value)}
+              placeholder='Calories'
+              className='w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor'   
+            />
+          </div>
+
+          <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+            <MdAttachMoney className='text-gray-700 text-2xl' />
+            <input 
+              type="text"
+              required
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              placeholder='Price'
+              className='w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor'   
+            />
+          </div>
+        </div>
+
+        <div className='w-full flex items-center'>
+          <button
+            type='button'
+            className='ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold'
+            onClick={saveDetails}
+          >
+            Save
+          </button>
         </div>
       </div>
     </div>
